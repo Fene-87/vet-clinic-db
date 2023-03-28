7da84494d113c06659bc79bd1d0e127c83245f85
Fene-87 <Fenekayas87@gmail.com>
2023-03-28 18:56:15 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id bigserial not null primary key
-    name varchar(100)
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg INT
 );
