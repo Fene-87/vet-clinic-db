719bc7733ba9327a0d937b6dfb18bcdcda5fed57
Fene-87 <Fenekayas87@gmail.com>
2023-03-30 15:41:15 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -23,3 +23,12 @@ SELECT * FROM animals;
 
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
