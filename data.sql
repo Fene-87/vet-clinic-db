6b65a08237acbe3f22ea6301cabde22f4611d005
Fene-87 <Fenekayas87@gmail.com>
2023-03-31 00:41:41 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -25,10 +25,16 @@ INSERT INTO species (name)
     VALUES ('Pokemon'),
            ('Digimon');
 
-UPDATE animals
-SET species_id = 2
-WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
 
-UPDATE animals
-SET species_id = 1
-WHERE species_id IS NULL;
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 where name = 'Agumon';
+
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
