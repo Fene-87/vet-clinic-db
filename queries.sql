cb631cf4fc393dbee8db2f12d6661acbc050957d
Fene-87 <Fenekayas87@gmail.com>
2023-03-30 16:06:52 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -38,3 +38,16 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT deleted_from_2022;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO deleted_from_2022;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
